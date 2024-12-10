f84605c7c9bafe08d8ef9c2f2f977eb0bc141e7e
MK <andreyka.anokhin@mail.ru>
2024-12-11 00:06:18 +0300

    Добавление функции для админа и написан первый декоратор который реализовывает обвертку функции и проверяет есть ди такой пользователь в bd

--- admin_handler/admin.py
+++ admin_handler/admin.py
@@ -1,3 +1,26 @@
-from aiogram import Router
+from functools import wraps
+
+from aiogram import Router, types
+from aiogram.filters import Command
 
 admin_router = Router()
+
+
+def is_admin(func):
+    @wraps(func)
+    async def wrapper(message: types.Message, *args, **kwargs):
+        # Написать функцию, которая будет доставать id админов и предоставлять доступ к этой команде
+        if message.from_user.id in ['ddd']:
+            await message.answer('Ок')
+            return await func(message, *args, **kwargs)
+        else:
+            await message.answer('Доступ ограничен к этой команде!')
+    return wrapper
+
+
+@admin_router.message(Command("add_user"))
+@is_admin
+async def add_user(message: types.Message):
+    """
+    """
+    await message.answer('ddd')
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import asyncio
 from dotenv import find_dotenv, load_dotenv
 import os
 
+from admin_handler.admin import admin_router
 from user_handler.user import router
 
 load_dotenv(find_dotenv())
@@ -10,6 +11,7 @@ load_dotenv(find_dotenv())
 bot = Bot(token=os.getenv('TOKEN'))
 
 dp = Dispatcher()
+dp.include_router(router=admin_router)
 dp.include_router(router=router)
 
 
